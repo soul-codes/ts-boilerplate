7ac84a3898e6f045c8c2805d693fe7bc6810b813
soul-codes <reach.soul.codes@gmail.com>
2018-06-19 00:04:14 +0200

    react demo setup

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -1,6 +1,16 @@
 module.exports = {
   mode: "development",
-  entry: "./demo/index.js",
+  entry: "./demo/index.jsx",
+  module: {
+    rules:
+      [
+        {
+          test: /\.js?/,
+          exclude: /node_modules/,
+          use: 'babel-loader?presets=react',
+        }
+      ]
+  },
   output: {
     filename: "demo.js"
   }
